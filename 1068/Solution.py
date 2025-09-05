e1db30507b7287b0e1a73d545fc39290e55a35e5
feTus <snow1013895225@gmail.com>
2025-09-05 13:37:34 +0800

    chore: add comment

--- 1068/Solution.py
+++ 1068/Solution.py
@@ -2,6 +2,9 @@ import pandas as pd
 
 
 def sales_analysis(sales: pd.DataFrame, product: pd.DataFrame) -> pd.DataFrame:
+    """
+    See: https://leetcode.cn/problems/product-sales-analysis-i/description/
+    """
     return pd.merge(sales, product, how="left", on="product_id").loc[
         :, ["product_name", "year", "price"]
     ]
