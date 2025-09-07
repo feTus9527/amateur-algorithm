f3dcfbbf4dd1ebec874db39a800efe7c57e31616
feTus <snow1013895225@gmail.com>
2025-09-07 18:18:43 +0800

    feat: implementation of the 'min cost climbing stairs' solution in python

---
+++ 0746/Solution.py
@@ -0,0 +1,18 @@
+from functools import cache
+from typing import List
+
+
+class Solution:
+    """
+    See: https://leetcode.cn/problems/min-cost-climbing-stairs/description/
+    """
+
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        _length = len(cost)
+        _f0 = 0
+        _f1 = 0
+        for i in range(2, _length + 1):
+            _f2 = min(_f1 + cost[i - 1], _f0 + cost[i - 2])
+            _f0 = _f1
+            _f1 = _f2
+        return _f1
