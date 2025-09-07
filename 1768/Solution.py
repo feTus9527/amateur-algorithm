a97758f943e82c68dc17b972780c8588a53a5f60
feTus <snow1013895225@gmail.com>
2025-09-07 18:31:33 +0800

    feat: implementation of the 'merge strings alternately' solution in python

---
+++ 1768/Solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    """
+    See: https://leetcode.cn/problems/merge-strings-alternately/description/
+    """
+
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        result = ""
+        i, j = 0, 0
+        while i < len(word1) and j < len(word2):
+            result += word1[i] + word2[j]
+            i += 1
+            j += 1
+        return result + word1[i:] + word2[j:]
