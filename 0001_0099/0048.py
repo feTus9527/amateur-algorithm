1cf0153b975c10610cda81b7641d8e7ae3bb1a36
feTus <snow1013895225@gmail.com>
2025-09-29 18:33:56 +0800

    feat: implementation of the 'rotate image' solution in python

---
+++ 0001_0099/0048.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class Solution:
+    """
+    See: https://leetcode.cn/problems/rotate-image/description/
+    """
+
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        n = len(matrix)
+        for i in range(n):
+            for j in range(i):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+        for i in range(n):
+            for j in range(n // 2):
+                matrix[i][j], matrix[i][n - j - 1] = matrix[i][n - j - 1], matrix[i][j]
